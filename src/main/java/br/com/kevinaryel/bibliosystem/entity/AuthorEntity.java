f3adb70923c53307bb62ebde0c35565e2ead5b00
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-20 04:38:45 -0300

    feat: adicionado exceptions

---
+++ src/main/java/br/com/kevinaryel/bibliosystem/entity/AuthorEntity.java
@@ -0,0 +1,2 @@
+package br.com.kevinaryel.bibliosystem.entity;public class AuthorEntity {
+}
---
+++ src/main/java/br/com/kevinaryel/bibliosystem/exception/BusinessRuleException.java
@@ -0,0 +1,9 @@
+package br.com.kevinaryel.bibliosystem.exception;
+
+public class BusinessRuleException extends Exception {
+
+        public BusinessRuleException(String message) {
+            super(message);
+        }
+
+}
