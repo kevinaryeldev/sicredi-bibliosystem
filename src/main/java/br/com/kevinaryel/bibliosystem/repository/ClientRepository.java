e44a31762f433ac69b0cdd9c89ed4695527dd589
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-20 19:10:36 -0300

    feat: Query para retornar Client pelo documento

--- src/main/java/br/com/kevinaryel/bibliosystem/repository/ClientRepository.java
+++ src/main/java/br/com/kevinaryel/bibliosystem/repository/ClientRepository.java
@@ -2,8 +2,12 @@ package br.com.kevinaryel.bibliosystem.repository;
 
 import br.com.kevinaryel.bibliosystem.entity.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<ClientEntity,Integer> {
+    @Query(value = "SELECT 1 FROM CLIENT WHERE DOCUMENT = :document",
+    nativeQuery = true)
+    ClientEntity findByDocument(Integer document);
 }
