d076a87ce41be8274fce9ea0163c613f4da85c02
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-22 10:52:56 -0300

    feat: Controller para livros

--- src/main/java/br/com/kevinaryel/bibliosystem/controller/BookController.java
+++ src/main/java/br/com/kevinaryel/bibliosystem/controller/BookController.java
@@ -1,2 +1,37 @@
-package br.com.kevinaryel.bibliosystem.controller;public class BookController {
+package br.com.kevinaryel.bibliosystem.controller;
+
+import br.com.kevinaryel.bibliosystem.exception.BusinessRuleException;
+import br.com.kevinaryel.bibliosystem.exception.NotFoundException;
+import br.com.kevinaryel.bibliosystem.request.BookCreateRequest;
+import br.com.kevinaryel.bibliosystem.response.BookResponse;
+import br.com.kevinaryel.bibliosystem.response.PageResponse;
+import br.com.kevinaryel.bibliosystem.service.BookService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/book")
+@AllArgsConstructor
+public class BookController {
+    private final BookService bookService;
+
+    @GetMapping("/listar")
+    public ResponseEntity<PageResponse<BookResponse>> list(Integer page, Integer size){
+        return bookService.list(page,size);
+    }
+
+    @GetMapping("/busca-por-id/{id}")
+    public ResponseEntity<BookResponse> findById(@PathVariable Integer id) throws NotFoundException {
+        return bookService.findById(id);
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<BookResponse> create(@RequestBody BookCreateRequest book) throws BusinessRuleException {
+        return bookService.create(book);
+    }
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) throws NotFoundException {
+        return bookService.delete(id);
+    }
 }
