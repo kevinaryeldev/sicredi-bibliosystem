10dab246c7e9146de1cd5a6a7eb6368caaa4cbd8
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-22 11:16:39 -0300

    refactor: removendo import não utilizado

--- src/main/java/br/com/kevinaryel/bibliosystem/repository/ClientRepository.java
+++ src/main/java/br/com/kevinaryel/bibliosystem/repository/ClientRepository.java
@@ -2,7 +2,6 @@ package br.com.kevinaryel.bibliosystem.repository;
 
 import br.com.kevinaryel.bibliosystem.entity.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
