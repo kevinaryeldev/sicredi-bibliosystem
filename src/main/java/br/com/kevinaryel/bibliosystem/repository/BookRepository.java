f44ffa1b78472e232ee18f606ff546b98ece3030
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-22 10:52:05 -0300

    feat: Repository para tabela de livros

--- src/main/java/br/com/kevinaryel/bibliosystem/repository/BookRepository.java
+++ src/main/java/br/com/kevinaryel/bibliosystem/repository/BookRepository.java
@@ -1,2 +1,10 @@
-package br.com.kevinaryel.bibliosystem.repository;public class BookRepository {
+package br.com.kevinaryel.bibliosystem.repository;
+
+import br.com.kevinaryel.bibliosystem.entity.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity,Integer> {
+
 }
