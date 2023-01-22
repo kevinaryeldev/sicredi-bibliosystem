283b47e060c69f96110c01467a147fc779bd56fb
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-22 10:50:12 -0300

    fix: removido import não utilizado

--- src/main/java/br/com/kevinaryel/bibliosystem/controller/ClientController.java
+++ src/main/java/br/com/kevinaryel/bibliosystem/controller/ClientController.java
@@ -9,7 +9,6 @@ import br.com.kevinaryel.bibliosystem.response.PageResponse;
 import br.com.kevinaryel.bibliosystem.service.ClientService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
