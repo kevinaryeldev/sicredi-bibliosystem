254e4e5a52e2b9b7ccf96aefb7ba798d97e22bf7
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-23 02:17:58 -0300

    refactor: mudança no nome dos endpoints

--- src/main/java/br/com/kevinaryel/bibliosystem/controller/ClientController.java
+++ src/main/java/br/com/kevinaryel/bibliosystem/controller/ClientController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/client")
+@RequestMapping("/cliente")
 @AllArgsConstructor
 public class ClientController {
 
--- src/main/java/br/com/kevinaryel/bibliosystem/controller/CopyController.java
+++ src/main/java/br/com/kevinaryel/bibliosystem/controller/CopyController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/copy")
+@RequestMapping("/exemplar")
 @AllArgsConstructor
 public class CopyController {
 
